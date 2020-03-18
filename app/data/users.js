795a41fc7d777c27e0ed0492670503f2294e7c68
Tatum Daily <tgd9019@outlook.com>
2020-03-18 15:56:48 -0500

    Testing Phase

--- app/data/users.js
+++ app/data/users.js
@@ -3,29 +3,29 @@
 
 const users = [
     {
-        "name": "Yvonne",
-        "photo": "",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        name: "Yvonne",
+        photo: "",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }, {
-        "name": "Monica",
-        "photo": "",
-        "scores":[2, 3, 5, 5, 1, 5, 5, 3, 4, 4]
+        name: "Monica",
+        photo: "",
+        scores:[2, 3, 5, 5, 1, 5, 5, 3, 4, 4]
     }, {
-        "name": "Alexis",
-        "photo": "",
-        "scores":[4, 4, 5, 5, 5, 2, 2, 3, 3, 3]
+        name: "Alexis",
+        photo: "",
+        scores:[4, 4, 5, 5, 5, 2, 2, 3, 3, 3]
     }, {
-        "name": "Ethan",
-        "photo": "",
-        "scores":[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
+        name: "Ethan",
+        photo: "",
+        scores:[5, 1, 4, 4, 5, 1, 2, 5, 4, 1]
     }, {
-        "name": "Carter",
-        "photo": "",
-        "scores":[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
+        name: "Carter",
+        photo: "",
+        scores:[3, 2, 6, 4, 5, 1, 2, 5, 4, 1]
     }, {
-        "name": "Jaque",
-        "photo": "",
-        "scores":[4, 2, 3, 4, 5, 5, 1, 2, 1, 5]
+        name: "Jaque",
+        photo: "",
+        scores:[4, 2, 3, 4, 5, 5, 1, 2, 1, 5]
     }
 ];
 
--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -1,9 +1,6 @@
-const users = require("../data/users");
+const users = require("../data/users.js");
 
 module.exports = function(app) {
-
-    // Displays all Users //
-    // ========================================= //
 
     app.get("/api/users", function(req, res) {
         
@@ -16,13 +13,67 @@ module.exports = function(app) {
 
     app.post("/api/users", function(req, res) {
         
-        var newUser = req.body;
-    
-        console.log(newUser);
-    
-        users.push(newUser);
-    
-        res.json(newUser);
+        var totalDifference = 0;
+
+        var match = {
+            name: "",
+            photo: "",
+            friendDifference: 1000
+        };
+
+        var userData = req.body;
+        var userName = userData.name;
+        var userScores = userData.scores;
+
+        var scores = userScores.map(function(item) {
+            return parseInt(item, 10);
+        });
+
+        userData = {
+            name: req.body.name,
+            photo: req.body.photo,
+            scores: scores
+        }
+
+        console.log("");
+        console.log("Name: " + userName);
+        console.log("User Score: " + userScores);
+
+        var sum = scores.reduce((a, b) => a + b, 0);
+
+        console.log("Sum of users score: " + sum);
+        console.log("Best match difference: " + match.friendDifference);
+        console.log("");
+
+        for (let i = 0; i < users.length; i++) {
+
+            console.log(users[i].name);
+            totalDifference = 0;
+            console.log("Total Diff: " + totalDifference);
+            console.log("Best match difference: " + match.friendDifference);
+
+            var bestFriendScore = users[i].scores.reduce((a, b) => a + b, 0);
+
+            console.log("Total friend score: " + bestFriendScore);
+            totalDifference += Math.abs(sum - bestFriendScore);
+            console.log("-------> --------> " + totalDifference);
+
+            if (totalDifference <= match.friendDifference) {
+
+                match.name = users[i].name;
+                match.photo = users[i].photo;
+                match.friendDifference = totalDifference;
+
+            }
+            
+            console.log(totalDifference + " Total Difference");
+        }
+
+        console.log(match);
+        users.push(userData);
+        console.log("New User Added");
+        console.log(userData);
+        res.json(match);
 
     });
-}
+};
